503f16e699f8621f4c6a2f3799fbf5587b324dd1
Jennifer Takagi <jennitakagi@gmail.com>
2024-04-14 15:45:49 +0200

    [feat] Add SignUn screen

---
+++ src/screens/SignUp.tsx
@@ -0,0 +1,59 @@
+import { VStack, Image, Text, Center, Heading, ScrollView } from "native-base";
+
+import LogoSvg from '@assets/logo.svg';
+import BackgroundImg from '@assets/background.png';
+
+import { Input } from "@components/Input";
+import { Button } from "@components/Button";
+
+export function SignUp() {
+  return (
+    <ScrollView contentContainerStyle={{ flexGrow: 1 }} showsVerticalScrollIndicator={false}>
+        <VStack flex={1} bg="gray.700" px={10} pb={16}>
+        <Image
+          source={BackgroundImg}
+          alt="People working out"
+          resizeMode="contain"
+          position="absolute"
+        />
+
+        <Center my={24}>
+          <LogoSvg />
+
+          <Text color="gray.100" fontSize="sm">
+            Train your mind and body.
+          </Text>
+        </Center>
+
+        <Center>
+          <Heading color="gray.100" fontSize="xl" mb={6} fontFamily="heading">
+            Create your account
+          </Heading>
+
+          <Input
+            placeholder="Nome"
+          />
+
+          <Input
+            placeholder="Email"
+            keyboardType="email-address"
+            autoCapitalize="none"
+
+          />
+          <Input
+            placeholder="Password"
+            secureTextEntry
+          />
+
+          <Button title="Create and log in" />
+        </Center>
+
+        <Button
+          title="Back to login"
+          variant="outline"
+          mt={24}
+        />
+      </VStack>
+    </ScrollView>
+  );
+}
