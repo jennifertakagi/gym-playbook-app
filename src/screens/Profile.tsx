fb12e2df6c849f0efb2af4545f904353741b69d3
Jennifer Takagi <jennitakagi@gmail.com>
2024-04-15 22:49:16 +0200

    [feat] Add user avatar on profile

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,11 +1,22 @@
-import { VStack } from 'native-base';
+import { Center, ScrollView, VStack } from 'native-base';
 
 import { ScreenHeader } from '@components/ScreenHeader';
+import { UserPhoto } from '@components/UserPhoto';
 
 export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title='Profile' />
+
+      <ScrollView>
+        <Center mt={6} px={10}>
+          <UserPhoto
+            source={{ uri: 'https://github.com/jennifertakagi.png' }}
+            alt="User avatar"
+            size={33}
+          />
+        </Center>
+      </ScrollView>
     </VStack>
   );
 }
