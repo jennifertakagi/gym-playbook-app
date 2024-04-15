aaae3ebd11364b8edf5f015a5c54774c57d97555
Jennifer Takagi <jennitakagi@gmail.com>
2024-04-15 22:37:45 +0200

    [feat] Add ExerciseCard component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,8 +1,9 @@
-import { FlatList, HStack, VStack } from 'native-base';
+import { useState } from 'react';
+import { FlatList, Heading, HStack, Text, VStack } from 'native-base';
 
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
-import { useState } from 'react';
+import { ExerciseCard } from '@components/ExerciseCard';
 
 export function Home() {
 
@@ -32,6 +33,20 @@ export function Home() {
         maxH={10}
       />
 
+      <VStack px={8}>
+        <HStack justifyContent="space-between" mb={5}>
+          <Heading color="gray.200" fontSize="md">
+            Exercicies
+          </Heading>
+
+          <Text color="gray.200" fontSize="sm">
+            4
+          </Text>
+        </HStack>
+
+        <ExerciseCard />
+        <ExerciseCard />
+      </VStack>
     </VStack>
   );
 }
