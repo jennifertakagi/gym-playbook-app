13c8fa362d3732e0d8c2382994f0e2fec14b1934
Jennifer Takagi <jennitakagi@gmail.com>
2024-04-15 22:34:25 +0200

    [feat] Implement selected group

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -2,15 +2,27 @@ import { HStack, VStack } from 'native-base';
 
 import { Group } from '@components/Group';
 import { HomeHeader } from '@components/HomeHeader';
+import { useState } from 'react';
 
 export function Home() {
+
+  const [groupSelected, setGroupSelected] = useState('arms')
+
   return (
     <VStack flex={1}>
       <HomeHeader />
 
       <HStack>
-        <Group name="Arms" />
-        <Group name="Legs" />
+        <Group
+          name="Arms"
+          isActive={groupSelected === 'arms'}
+          onPress={() => setGroupSelected('arms')}
+        />
+        <Group
+          name="Legs"
+          isActive={groupSelected === 'legs'}
+          onPress={() => setGroupSelected('legs')}
+        />
       </HStack>
 
     </VStack>
