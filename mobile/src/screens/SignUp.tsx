77c8d3009945d8eb58978b8c236f264bc42526ad
Jennifer Takagi <jennitakagi@gmail.com>
2024-04-19 20:52:49 +0200

    [feat] Setup Error class and interceptors in api

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -1,8 +1,7 @@
-import { VStack, Image, Text, Center, Heading, ScrollView, Alert } from "native-base";
+import { VStack, Image, Text, Center, Heading, ScrollView, useToast } from "native-base";
 import { useForm, Controller } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
-import axios from 'axios';
 
 import LogoSvg from '@assets/logo.svg';
 import BackgroundImg from '@assets/background.png';
@@ -12,6 +11,7 @@ import { Button } from "@components/Button";
 import { useNavigation } from "@react-navigation/native";
 
 import { api } from "@services/api";
+import { AppError } from '@utils/AppError';
 
 type FormDataProps = {
   name: string;
@@ -29,6 +29,7 @@ const signUpSchema = yup.object({
 
 export function SignUp() {
   const navigation = useNavigation();
+  const toast = useToast();
 
   const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
     resolver: yupResolver(signUpSchema),
@@ -43,9 +44,15 @@ export function SignUp() {
       const response = await api.post('/users', { name, email, password });
       console.log(response.data);
     } catch (error) {
-      if (axios.isAxiosError(error)) {
-        Alert(error.response?.data.message);
-      }
+      const isAppError = error instanceof AppError;
+
+      const title = isAppError ? error.message : 'Error on creating an account. Try again later!';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
     }
   }
 
