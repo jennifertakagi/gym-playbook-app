893b51435eb60cf4608dd28e2f67576ade9862dd
Jennifer Takagi <jennitakagi@gmail.com>
2024-04-19 20:56:17 +0200

    [feat] Add AuthContext

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,6 +2,8 @@ import { StatusBar } from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
+import { AuthContext } from '@contexts/AuthContext';
+
 import { Routes } from '@routes/index';
 
 import { THEME } from './src/theme';
@@ -18,7 +20,15 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Routes /> : <Loading />}
-    </NativeBaseProvider>
+       <AuthContext.Provider value={{
+        user: {
+          avatar: 'jennifer.png',
+          email: 'jennifer@email.com',
+          id: '1',
+          name: 'Jennifer Takagi',
+        }
+      }}>
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </AuthContext.Provider>    </NativeBaseProvider>
   );
 }
--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,10 +1,16 @@
 import { useTheme, Box } from 'native-base';
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+import { useContext } from 'react';
+
+import { AuthContext } from '@contexts/AuthContext';
 
 import { AuthRoutes } from './auth.routes';
 
 export function Routes() {
   const { colors } = useTheme();
+  const contextData = useContext(AuthContext);
+
+  console.log("USUÁRIO LOGADO =>", contextData);
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
