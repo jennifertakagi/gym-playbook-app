e82fc96d35c76404946120de61b813af83751e9f
Jennifer Takagi <jennitakagi@gmail.com>
2024-04-19 21:19:18 +0200

    [feat] Show loading when getting user data

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,6 +1,8 @@
 import { useTheme, Box } from 'native-base';
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 
+import { Loading } from '@components/Loading';
+
 import { useAuth } from '@hooks/useAuth';
 
 import { AuthRoutes } from './auth.routes';
@@ -8,10 +10,14 @@ import { AppRoutes } from './app.routes';
 
 export function Routes() {
   const { colors } = useTheme();
-  const { user } = useAuth();
+  const { user, isLoadingUserStorageData } = useAuth();
 
   const theme = DefaultTheme;
   theme.colors.background = colors.gray[700];
+
+  if (isLoadingUserStorageData) {
+    return <Loading />
+  }
 
   return (
     <Box flex={1} bg="gray.700">
