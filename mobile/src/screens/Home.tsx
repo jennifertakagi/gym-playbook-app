f34b92125fd86e70f5055a627f88127e255a7e12
Jennifer Takagi <jennitakagi@gmail.com>
2024-04-21 22:31:00 +0200

    [feat] Add type to exercise

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -10,11 +10,12 @@ import { HomeHeader } from '@components/HomeHeader';
 import { ExerciseCard } from '@components/ExerciseCard';
 
 import { AppNavigatorRoutesProps } from '@routes/app.routes';
+import { ExerciseDTO } from '@dtos/ExerciseDTO';
 
 export function Home() {
 
   const [groups, setGroups] = useState<string[]>([]);
-  const [exercises, setExercises] = useState([]);
+  const [exercises, setExercises] = useState<ExerciseDTO[]>([]);
   const [groupSelected, setGroupSelected] = useState('Costas');
 
   const toast = useToast();
@@ -44,8 +45,8 @@ export function Home() {
   async function fecthExercisesByGroup() {
     try {
       const response = await api.get(`/exercises/bygroup/${groupSelected}`);
-      console.log(response.data);
 
+      setExercises(response.data);
     } catch (error) {
       const isAppError = error instanceof AppError;
       const title = isAppError ? error.message : 'Error on listing exercises. Try again later!';
@@ -104,7 +105,7 @@ export function Home() {
 
         <FlatList
           data={exercises}
-          keyExtractor={item => item}
+          keyExtractor={item => item.id}
           renderItem={({ item }) => (
             <ExerciseCard onPress={handleOpenExerciseDetails} />
           )}
